354188c50c09ba05dede565fd11c9f2af0ac50fa
Bartek Zdanowski <bzd@touk.pl>
2013-10-15 17:32:02 +0200

    shared data

---
+++ src/main/java/shared_data_safe/shared_data/DataRepo.java
@@ -0,0 +1,7 @@
+package shared_data_safe.shared_data;
+
+/**
+ * @author bzd
+ */
+public class DataRepo {
+}
---
+++ src/main/java/shared_data_safe/shared_data/Starter.java
@@ -0,0 +1,14 @@
+package shared_data_safe.shared_data;
+
+import org.vertx.java.platform.Verticle;
+
+/**
+ * @author bzd
+ */
+public class Starter extends Verticle {
+    @Override
+    public void start() {
+        container.deployVerticle("shared_data/Producer.java");
+        container.deployVerticle("shared_data/Consumer.java", 5);
+    }
+}
